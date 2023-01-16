262ba26fe8135af381d6e4f4c81e2149a7c64b01
Семён Крекотун <samkrekotyn@gmail.com>
2023-01-16 23:34:17 +0300

    header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,34 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+const menuToggle = document.querySelector(".icon-menu");
+const siteNavigation = document.querySelector(".menu__body");
+
+menuToggle.addEventListener("click", () =>
+{
+	const isOpened = menuToggle.getAttribute("aria-expanded") === "true";
+	if (isOpened ? closeMenu() : openMenu());
+});
+
+function openMenu()
+{
+	menuToggle.setAttribute("aria-expanded", "true");
+	siteNavigation.setAttribute("data-state", "opened");
+}
+function closeMenu()
+{
+	menuToggle.setAttribute("aria-expanded", "false");
+	siteNavigation.setAttribute("data-state", "closing");
+
+	siteNavigation.addEventListener(
+		"animationend",
+		() =>
+		{
+			siteNavigation.setAttribute("data-state", "closed");
+		},
+		{ once: true }
+	);
+}
+console.log(menuToggle)
