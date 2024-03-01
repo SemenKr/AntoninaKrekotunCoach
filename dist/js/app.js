f223eba1d29c74609d314ee36ed6e0cb1298c706
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2024-03-01 18:18:19 +0300

    slider fix

--- dist/js/app.js
+++ dist/js/app.js
@@ -4801,58 +4801,6 @@
             once: true
         });
     }
-    document.addEventListener("DOMContentLoaded", (function() {
-        const form = document.getElementById("form");
-        form.addEventListener("submit", formSend);
-        async function formSend(e) {
-            e.preventDefault();
-            let error = formValidate(form);
-            let formData = new FormData(form);
-            if (0 === error) {
-                form.classList.add("_sending");
-                let response = await fetch("sendmail.php", {
-                    method: "POST",
-                    body: formData
-                });
-                if (response.ok) {
-                    let result = await response.json();
-                    alert(result.message);
-                    form.reset();
-                    form.classList.remove("_sending");
-                }
-            } else alert("Что-то пошло не так");
-            alert("Заполните обязательные поля");
-        }
-        function formValidate(form) {
-            let error = 0;
-            let formReq = document.querySelectorAll("._req");
-            for (let index = 0; index < formReq.length; index++) {
-                const input = formReq[index];
-                formRemoveError(input);
-                if (input.classList.contains("_email")) if (emailTest(input)) {
-                    formAddError(input);
-                    error++;
-                } else if ("checkbox" === input.getAttribute("type") && false === input.checked) {
-                    formAddError(input);
-                    error++;
-                } else if ("" === input.value) {
-                    formAddError(input);
-                    error++;
-                }
-            }
-        }
-        function formAddError(input) {
-            input.parentElement.classList.add("._error");
-            input.classList.add("._error");
-        }
-        function formRemoveError(input) {
-            input.parentElement.classList.remove("._error");
-            input.classList.remove("._error");
-        }
-        function emailTest(input) {
-            return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-        }
-    }));
     window["FLS"] = true;
     isWebp();
     menuInit();
